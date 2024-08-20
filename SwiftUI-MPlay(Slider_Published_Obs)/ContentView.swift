0c67a12fa71ce5dc7bf622b3248f6d28381c7023
Evhen <35118152+Ribeyrak@users.noreply.github.com>
2024-08-19 20:53:55 -0400

    Initial Commit

---
+++ SwiftUI-MPlay(Slider_Published_Obs)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-MPlay(Slider_Published_Obs)
+//
+//  Created by Evhen Lukhtan on 19.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-MPlay(Slider_Published_Obs)/SwiftUI_MPlay_Slider_Published_Obs_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MPlay_Slider_Published_Obs_App.swift
+//  SwiftUI-MPlay(Slider_Published_Obs)
+//
+//  Created by Evhen Lukhtan on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MPlay_Slider_Published_Obs_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
